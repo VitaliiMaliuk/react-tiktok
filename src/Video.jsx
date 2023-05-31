5d3da38993979807ff9e606fd6f133ceff4472cf
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2023-05-31 17:40:36 +0300

    {

--- src/Video.jsx
+++ src/Video.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export default function Video(props) {
-    
+    const { url } = props;
     return (
         <div className="video">
-            <video className="player" loop src={props.url}></video>
+            <video className="player" loop src={url}></video>
         </div>
     )
 }
